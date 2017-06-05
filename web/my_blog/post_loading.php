762da32bd6558ef2b30d5eccc125ad7e3e688142
FRANK RINCON <rin16002@byui.edu>
2017-06-05 14:29:15 -0600

    almost done2

--- web/my_blog/post_loading.php
+++ web/my_blog/post_loading.php
@@ -6,11 +6,11 @@ include "connect.php";
 $secret_content = htmlspecialchars($_POST["secret_content"]);
 $university = htmlspecialchars($_POST["university"]);
 
-$insert_query = $db->prepare('INSERT INTO post(content, college_id, likes, dislikes, sent_date) 
+$insert_query = $db->prepare('INSERT INTO post(content, college_id, sent_date) 
                                         VALUES
                                         (:secret_content
                                         , :college_id
-                                        , 0, 0, CURRENT_DATE)');
+                                        , CURRENT_DATE)');
 
 $insert_query->execute(array(':secret_content' => $secret_content, ':college_id' => $university));
 
